c4ac86e15a75570cfc235a57db1a3c1b07c641d7
UraGun <116074290+LevAlekseev@users.noreply.github.com>
2024-09-17 12:57:29 +0800

    Update Lab2_Alekseev.pl

--- Lab2_Alekseev.pl
+++ Lab2_Alekseev.pl
@@ -1,3 +1,5 @@
+task #1
+
 rangConcat([], List2, List2):-!.
 rangConcat(List1, [], List1):-!.
 rangConcat([Head1|Tail1], [Head2|Tail2], [Head1|TailResult]):-
@@ -5,3 +7,14 @@ rangConcat([Head1|Tail1], [Head2|Tail2], [Head1|TailResult]):-
   
 rangConcat(List1, [Head2|Tail2], [Head2|TailResult]):-
    rangConcat(List1, Tail2, TailResult).
+
+
+task #2
+
+is_even(X) :- X mod 2 =:= 0.
+even_elements([], []).
+even_elements([Head|Tail], [Head|Result]) :-
+    is_even(Head),
+    even_elements(Tail, Result).
+even_elements([_Head|Tail], Result) :-
+    even_elements(Tail, Result).
